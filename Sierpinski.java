b72ae36075773bd2795ced20a9e0c42c227bfc98
Sean Thomas <spthomas127@gmail.com>
2022-01-28 12:26:01 -0500

    Initial commit

---
+++ Sierpinski.java
@@ -0,0 +1,51 @@
+public class Sierpinski {
+
+    // Height of an equilateral triangle whose sides are of the specified length. 
+    public static double height(double length) {
+        double height = length * Math.sqrt(3) / 2;
+        return height;
+    }
+
+    // Draws a filled equilateral triangle whose bottom vertex is (x, y) 
+    // of the specified side length. 
+    public static void filledTriangle(double x, double y, double length) {
+        double x1 = x;
+        double y1 = y;
+        double x2 = x - (length / 2);
+        double y2 = y + height(length);
+        double x3 = x + (length / 2);
+        double y3 = y + height(length);
+
+        double[] xVals = new double[]{x1, x2, x3};
+        double[] yVals = new double[]{y1, y2, y3};
+
+        StdDraw.filledPolygon(xVals, yVals);
+    }
+
+    // Draws a Sierpinski triangle of order n, such that the largest filled 
+    // triangle has bottom vertex (x, y) and sides of the specified length. 
+    public static void sierpinski(int n, double x, double y, double length) {
+        if (n == 0) {
+            return;
+        }
+    
+        else {
+            filledTriangle(x, y, length);
+            sierpinski(n - 1, x - (length / 2), y, length / 2);
+            sierpinski(n - 1, x + (length / 2), y, length / 2);
+            sierpinski(n - 1, x, y + height(length), length / 2);
+        }
+    }
+
+    // Takes an integer command-line argument n; 
+    // draws the outline of an equilateral triangle (pointed upwards) of length 1; 
+    // whose bottom-left vertex is (0, 0) and bottom-right vertex is (1, 0); and 
+    // draws a Sierpinski triangle of order n that fits snugly inside the outline. 
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        double[] xVals = new double[]{0, .5, 1};
+        double[] yVals = new double[]{0, height(1), 0};
+        StdDraw.polygon(xVals, yVals);
+        sierpinski(n, 0.5, 0, 0.5);
+    }
+}
